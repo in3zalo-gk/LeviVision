ea2df6a2948975262d3e48fd0fcd1b26f26fd629
in3zalo-gk <taina.tvsm2021@gmail.com>
2026-07-26 21:04:14 -0300

    Atualiza projeto LeviVision

--- src/Config.cpp
+++
@@ -1,9 +0,0 @@
-#include "Config.h"
-
-nlohmann::json makeDefaultConfigJson() {
-    return pl::config::defaultJson(LeviVisionConfig{});
-}
-
-nlohmann::json makeConfigSchemaJson() {
-    return pl::config::schema(LeviVisionConfig{});
-}
--- src/Main.cpp
+++
@@ -1,5 +0,0 @@
-#include "LeviVision.h"
-
-#include <pl/Mod.hpp>
-
-PL_REGISTER_MOD(LeviVision, LeviVision::instance())
